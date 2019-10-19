a092f2eaed5de40c358ec5ac551d1c7344b219d6
Joseph-Bollea-Peano-4A <56471124+Joseph-Bollea-Peano-4A@users.noreply.github.com>
2019-10-19 09:47:14 +0200

    versione 1.0

---
+++ Calcolo.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calcolo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author joseph.bollea
+ */
+public class Calcolo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int b, h, area, perimetro;
+        b = Integer.parseInt(JOptionPane.showInputDialog("inserisci la base: "));
+        h = Integer.parseInt(JOptionPane.showInputDialog("inserisci l'altezza: "));
+        System.out.println("Area: " + calcolo(b, h, 'a'));
+        System.out.println("Perimetro: " + calcolo(b, h, 'p'));
+
+    }
+
+    public static int calcolo(int b, int h, char c) {
+        switch (c) {
+            case 'p':
+                return 2 * (b + h);
+            case 'a':
+                return b * h;
+            default:
+                return 0;
+        }
+
+    }
+
+}
